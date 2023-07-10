38a1094b785765828a7bc64b52d18385ae4384f4
tabira2382 <tabira2382@gmail.com>
2023-07-10 15:37:08 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+  const [editIndex, setEditIndex] = useState(null);
+
+  const handleAddTodo = () => {
+    setTodos([...todos, { text: input, completed: false }]);
+    setInput('');
+  };
+
+  const handleDeleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  const handleEditTodo = (index) => {
+    setInput(todos[index].text);
+    setEditIndex(index);
+  };
+
+  const handleUpdateTodo = () => {
+    const newTodos = [...todos];
+    newTodos[editIndex].text = input;
+    setTodos(newTodos);
+    setInput('');
+    setEditIndex(null);
+  };
+
+  const handleToggleComplete = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={input} onChange={e => setInput(e.target.value)} />
+      <button onClick={editIndex === null ? handleAddTodo : handleUpdateTodo}>
+        {editIndex === null ? 'Add' : 'Update'}
+      </button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index} style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+            {todo.text}
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+            <button onClick={() => handleEditTodo(index)}>Edit</button>
+            <button onClick={() => handleToggleComplete(index)}>Complete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
